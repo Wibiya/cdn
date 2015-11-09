e53ca3cc7e7225e3353d78a441ead6744f5af1a7
Damodar Bashyal <dbashyal@gmail.com>
2015-11-09 21:24:53 +1100

    Update wibiya.js

--- wibiya.js
+++ wibiya.js
@@ -18,14 +18,22 @@
       }
   };
   loadWibiya(function($) {
+    var $wibiya = $('<div class="wibiyaDotComNewBarNotice"/>');
     $(function() {
       var $body = $('body');
-      var $wibiya = $('<div class="wibiyaDotComNewBarNotice"/>');
       $body.append($wibiya);
-      var $css = '<style type="text/css">.wibiyaDotComNewBarNotice {background: #cddcdd;border-radius: 0 !important;border: 0 !important;bottom: 0;display: block;font: 12px Arial, Helvetica, sans-serif;font-size: 12px;font-weight: normal;height: 40px;left: 0;line-height: 1;margin: 0 !important;padding: 0 !important;position: fixed;width: 100%;}</style>';
+      var $css = '<style type="text/css">.wibiyaDotComNewBarNotice {background: none transparent;border-radius: 0 !important;border: 0 !important;bottom: 0;display: block;font: 12px Arial, Helvetica, sans-serif;font-size: 12px;font-weight: normal;height: 90px;left: 0;line-height: 1;margin: 0 !important;padding: 0 !important;position: fixed;width: 100%;}</style>';
       // $css = $css.replace(/\s+/g, " "); // removes new line, tabs and spaces
       $('head').append($css);
     });
+
+    if (!sessionStorage.alreadyLoaded) {
+      /* Just load once per session */
+      $wibiya.html('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- wibiya --> <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9567128729272204" data-ad-slot="2220424816" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>');
+      sessionStorage.alreadyLoaded = 1;
+    } else {
+      $wibiya.remove();
+    }
   });
 })();
 /*(function( $ ) {})(jQuery);*/
